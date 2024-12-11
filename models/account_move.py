82f485f81d0b05bff86d70885fa1a674abd5ac81
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 08:51:46 +0700

    Fix salesman fields

--- models/account_move.py
+++ models/account_move.py
@@ -3,15 +3,12 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    employee_id = fields.Many2many(
-        'hr.employee',
-        string='Salesman'
-    )
+    employee_ids = fields.Many2many('hr.employee', string="Salesmen")
 
     @api.model
     def create(self, vals):
         if 'invoice_origin' in vals:
             sale_order = self.env['sale.order'].search([('name', '=', vals['invoice_origin'])], limit=1)
             if sale_order:
-                vals['employee_id'] = sale_order.employee_id.id
+                vals['employee_ids'] = [(6, 0, sale_order.employee_ids.ids)]
         return super(AccountMove, self).create(vals)
