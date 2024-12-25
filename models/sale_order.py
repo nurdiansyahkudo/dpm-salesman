cd75cc7d89c758df4b5c31fefd700fe138447ec4
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-25 15:20:29 +0700

    Fix Salesman for Commission

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,16 +3,16 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # employee_id = fields.Many2many(
-    #     'hr.employee',
-    #     string="Salesman"
-    # )
-    employee_id = fields.Many2one(
-        'res.partner',
-        string='Salesman',
-        domain=[
-            ('is_company', '=', False), 
-            ('company_name', '=', 'Duta Pertiwi Mandiri')
-        ],
-        readonly=True
+    employee_id = fields.Many2many(
+        'hr.employee',
+        string="Salesman"
     )
+    # employee_id = fields.Many2one(
+    #     'res.partner',
+    #     string='Salesman',
+    #     domain=[
+    #         ('is_company', '=', False), 
+    #         ('company_name', '=', 'Duta Pertiwi Mandiri')
+    #     ],
+    #     readonly=True
+    # )
